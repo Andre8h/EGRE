f7b918344c01903c939b349c937d834158b8c5cc
Andre <andresquinones71@gmail.com>
2025-02-15 21:20:33 -0500

    Creation and Serialization of Books model, Creation of the Navbar component

--- EGREBD/books/models.py
+++ EGREBD/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Books(models.Model):
+    Title = models.CharField(max_length=50 , null = False)
+    AddDate = models.DateField( auto_now=False, auto_now_add=True)
+    Author = models.CharField(max_length=30, null = True)
+
+    def __str__(self):
+        return self.Title
+    
 # Create your models here.
